19a28d5c3e481f2cc9a2ced40a9a17093233b77d
Ronald1507 <rpfu1999@gmail.com>
2025-07-31 01:35:32 -0400

    fix iconos

--- src/js/iconos.js
+++ src/js/iconos.js
@@ -3,9 +3,12 @@ import lottie from "lottie-web";
 
 export function cargarIconos() {
 	const iconos = [
-		{ id: "icono-calendar", path: "/assets/animations/calendario.json" },
-		{ id: "icono-regalo", path: "/assets/animations/regalo_dos.json" },
-		{ id: "icono-gps", path: "/assets/animations/icono-gps.json" },
+		{
+			id: "icono-calendar",
+			path: "/src/assets/animations/calendario.json",
+		},
+		{ id: "icono-regalo", path: "/src/assets/animations/regalo_dos.json" },
+		{ id: "icono-gps", path: "/src/assets/animations/icono-gps.json" },
 
 		// agrega más iconos aquí si quieres
 	];
