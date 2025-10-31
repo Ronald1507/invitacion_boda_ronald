31976cb319cfaf5ba8576291dfe904646d892e04
Ronald1507 <rpfu1999@gmail.com>
2025-10-31 15:23:40 -0300

    add fotos animadas

--- src/js/swiperSetup.js
+++ src/js/swiperSetup.js
@@ -1,14 +1,15 @@
 // swiperSetup.js
 import Swiper from "swiper";
-import { Autoplay, Pagination } from "swiper/modules";
+import { Autoplay, Pagination, Navigation } from "swiper/modules"; // 👈 Agregado Navigation
 
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation"; // 👈 Importa también el CSS del módulo Navigation
 
 // Inicializar Swiper con módulos
 export function initSwiper() {
 	new Swiper(".mySwiper", {
-		modules: [Autoplay, Pagination], // 🔥 Módulos obligatorios
+		modules: [Autoplay, Pagination], // 👈 Incluye Navigation
 		loop: true,
 		spaceBetween: 16,
 		centeredSlides: true,
